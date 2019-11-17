45112e65a77cee42f7534c198a0aed33feadc12a
appaposentaf@outlook.com <appaposentaf@outlook.com>
2019-11-17 14:34:41 -0300

    Configurado o Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'ceb730c93afc06',
+    pass: '934b81d89380b8',
+  },
+  default: {
+    from: 'Equipe nomeDaBarbearia <noreply@app.com>',
+  },
+};
