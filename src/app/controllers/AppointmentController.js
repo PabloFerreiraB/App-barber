a7812f72e5dfc879eb7865a675e18e6f96bee320
appaposentaf@outlook.com <appaposentaf@outlook.com>
2019-11-17 13:08:09 -0300

    Notificando novos agendamentos / Listando notificações do usuário /

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
-import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
+// import CancellationMail from '../jobs/CancellationMail';
+// import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -13,8 +15,8 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
-      limite: 20,
+      attributes: ['id', 'date', 'past', 'cancelable'],
+      limit: 20,
       offset: (page - 1) * 20,
       include: [
         {
@@ -22,17 +24,12 @@ class AppointmentController {
           as: 'provider',
           attributes: ['id', 'name'],
           include: [
-            {
-              model: File,
-              as: 'avatar',
-              attributes: ['id', 'path', 'url'],
-            },
+            { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
           ],
         },
       ],
     });
-
-    return res.json(appointments);
+    res.json(appointments);
   }
 
   async store(req, res) {
@@ -44,29 +41,29 @@ class AppointmentController {
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Falha nas validações' });
     }
-
     const { provider_id, date } = req.body;
 
-    // Verificar se provider_id é um provedor de serviço
-    const isProvider = await User.findOne({
+    // Verifique se provider_id é um provedor
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
 
-    if (!isProvider) {
-      return res
-        .status(401)
-        .json({ error: 'Você só pode criar compromissos com fornecedores' });
+    if (!checkIsProvider) {
+      return res.status(401).json({
+        error: 'Você só pode criar agendamentos com prestadores de serviço',
+      });
     }
 
-    // Verificando se a data já passou
+    // Verifique as datas passadas
     const hourStart = startOfHour(parseISO(date));
+
     if (isBefore(hourStart, new Date())) {
       return res
-        .status(400)
+        .status(401)
         .json({ error: 'Datas passadas não são permitidas' });
     }
 
-    // Verificando disponibilidade da data de agendamento
+    // Verifique a disponibilidade da data
     const checkAvailability = await Appointment.findOne({
       where: {
         provider_id,
@@ -77,27 +74,70 @@ class AppointmentController {
 
     if (checkAvailability) {
       return res
-        .status(400)
-        .json({ error: 'Data para agendamento não disponível' });
+        .status(401)
+        .json({ error: 'A data do agendamento não está disponível' });
     }
-
-    // Criando o agendamento
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date,
     });
 
-    // Notificar o prestador de serviço
+    // Notificar prestador de serviço
     const user = await User.findByPk(req.userId);
-    const formattedDate = format(hourStart, "'dia' dd 'de' MMM', às' H:mm'h'", {
-      locale: pt,
-    });
+
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' HH:mm'h'",
+      {
+        locale: pt,
+      }
+    );
 
     await Notification.create({
-      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'Você não tem permissão para cancelar este agendamento',
+      });
+    }
+
+    // const dateWithSub = subHours(appointment.date, 2);
+
+    // if (isBefore(dateWithSub, new Date())) {
+    //   return res.status(401).json({
+    //     error: 'Você só pode cancelar agendamentos com 2 horas de antecedência',
+    //   });
+    // }
+
+    // appointment.canceled_at = new Date();
+
+    // await appointment.save();
+
+    // await Queue.add(CancellationMail.key, {
+    //   appointment,
+    // });
 
     return res.json(appointment);
   }
