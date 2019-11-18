905a8e9d88df808672a53eb4d54cf6c18532dd9a
appaposentaf@outlook.com <appaposentaf@outlook.com>
2019-11-18 16:50:03 -0300

    Ajustes

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   // CRIAÇÃO DO USUÁRIO
@@ -68,13 +69,23 @@ class UserController {
       return res.status(401).json({ error: 'As senhas não conferem' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'url', 'path'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
